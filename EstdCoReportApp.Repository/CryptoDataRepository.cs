04cb5b430c5489b27465e664bf651df041756fb7
majid dej <majiddej@hotmail.com>
2023-12-23 22:09:51 +0330

    تغییرات ظاهری

---
+++ EstdCoReportApp.Repository/CryptoDataRepository.cs
@@ -0,0 +1,45 @@
+﻿using EstdCoReportApp.Application.Domain;
+using MongoDB.Driver;
+
+namespace EstdCoReportApp.Repository
+{
+    public class MongoDbDataRepository<T>
+    {
+        private readonly IMongoCollection<T> _collection;
+
+        public MongoDbDataRepository(IMongoDatabase database, string collectionName)
+        {
+            _collection = database.GetCollection<T>(collectionName);
+        }
+
+        public async Task InsertAsync(T entity)
+        {
+            await _collection.InsertOneAsync(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            var entities = await _collection.Find(Builders<T>.Filter.Empty).ToListAsync();
+            return entities;
+        }
+
+        // Add more CRUD operations as needed
+
+        // For example:
+        // public async Task<T> GetByIdAsync(string id)
+        // {
+        //     var entity = await _collection.Find(Builders<T>.Filter.Eq("_id", id)).FirstOrDefaultAsync();
+        //     return entity;
+        // }
+
+        // public async Task UpdateAsync(string id, T entity)
+        // {
+        //     await _collection.ReplaceOneAsync(Builders<T>.Filter.Eq("_id", id), entity);
+        // }
+
+        public async Task DeleteAsync(string id)
+        {
+            await _collection.DeleteOneAsync(Builders<T>.Filter.Eq("_id", id));
+        }
+    }
+}
